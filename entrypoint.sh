e325c2e303461ea043c46f3966f6d8ff600254b1
Brian Shaw <brian@beclicking.com>
2024-05-01 12:57:54 -0500

    add rewrite rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,51 @@ echo "Building application"
 #Execute the command provided
 `echo $INPUT_BUILD_COMMAND` 
 
-echo "aws --region $INPUT_AWS_DEFAULT_REGION s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME --no-progress --delete"
+# echo "aws --region $INPUT_AWS_DEFAULT_REGION s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME --no-progress --delete"
 #Sync files with amazon s3 bucket app
 aws --region $INPUT_AWS_DEFAULT_REGION s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME --no-progress --delete
+
+redirect_rules_file="redirect_rules.json"
+cat <<EOF > "$redirect_rules_file"
+{
+  "RoutingRules": [
+EOF
+
+i=0
+filetype="js"
+
+ls ./dist/assets | grep "index" | while read -r line ; do
+    echo "Processing $line"
+    # your code goes here
+    if [ $i -gt 0 ]; then
+      cat <<EOF >> "$redirect_rules_file"
+      ,
+EOF
+    fi
+
+if [[ $line == *"css"* ]]; then
+  filetype="css"
+fi
+
+filenamefiletype="index.$filetype"
+
+cat <<EOF >> "$redirect_rules_file"
+      {
+        "Condition": {
+          "KeyPrefixEquals": "assets/$filenamefiletype"
+        },
+        "Redirect": {
+          "ReplaceKeyWith": "assets/$line"
+        }
+      }
+EOF
+i=$(( i + 1 ))
+done
+cat <<EOF >> "$redirect_rules_file"
+  ]
+}
+EOF
+
+aws s3api put-bucket-website --region $INPUT_AWS_DEFAULT_REGION --bucket "$INPUT_AWS_BUCKET_NAME" --website-configuration "file://$redirect_rules_file"
+# Clean up the temporary JSON file
+rm "$redirect_rules_file"
